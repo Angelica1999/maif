83c272e28851b34d561d9dcddb07ab6515fc2176
gonzalesjhondy <gonzalesjhondy>
2023-10-24 17:09:32 +0800

    update from ewt and vat

--- resources/views/facility/edit.blade.php
+++ resources/views/facility/edit.blade.php
@@ -47,13 +47,13 @@
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="vat">Vat</label>
-                    <input type="text" class="form-control" id="vat" name="vat" value="{{ $facility->vat }}" placeholder="Vat" required>
+                    <input type="number" class="form-control" id="vat" name="vat" value="{{ $facility->vat }}" placeholder="Vat" required>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="Ewt">Ewt</label>
-                    <input type="text" class="form-control" id="Ewt" name="Ewt" value="{{ $facility->Ewt }}" placeholder="Ewt" required>
+                    <input type="number" class="form-control" id="Ewt" name="Ewt" value="{{ $facility->Ewt }}" placeholder="Ewt" required>
                 </div>
             </div>
       </div>       
