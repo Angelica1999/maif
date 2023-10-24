5a9f51088da26d483e83a11b977f009f8f2fb021
gonzalesjhondy <gonzalesjhondy>
2023-10-24 17:04:07 +0800

    update for print patient

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const MAIF = '/home';
     public const BUDGET = '/fundsource';
 
     /**
